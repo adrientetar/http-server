fdac3e69865908c27dc696be30609ef6fbb9e4ef
Adrien Tétar <adri-from-59@hotmail.fr>
2018-12-28 12:53:00 +0100

    Prettify

--- src/Runner.scala
+++ src/Runner.scala
@@ -1,21 +1,18 @@
 import java.nio.file.{Files, NoSuchFileException, Paths}
 
-import objects.{ContentType, HttpHeaders, HttpServer, HttpStatus}
+import objects._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success, Try}
 
 object Runner {
-  def fail(e: Throwable): Unit =
-    println(s"Failed: $e")
-
   def main(args: Array[String]): Unit = {
     val basepath: String = args match {
       case Array(p, _*) => p
       case _ =>
         println("Usage: runner <path>")
         System.exit(1)
-        throw new Exception(/* typeck */)
+        /* typeck */ throw new Exception()
     }
 
     val address = "127.0.0.1"
@@ -36,7 +33,7 @@ object Runner {
           }
           val path = Paths.get(basepath, uripath)
 
-          Try(Files.readAllBytes(path)) match {
+          val msg = Try(Files.readAllBytes(path)) match {
             case Success(content) =>
               val ext = uripath.lastIndexOf(".") match {
                 case ix if ix >= 0 && uripath.length() > ix => uripath.substring(ix+1)
@@ -44,25 +41,19 @@ object Runner {
               }
               response.headers.set(HttpHeaders.contentTypeHeader, ContentType.mimeType(ext))
               response.write(content)
-              response.send() andThen {
-                case Success(_) => println("Sent page! \uD83D\uDE80")
-                case Failure(e) => fail(e)
-              }
-            case Failure(ex: NoSuchFileException) =>
-              println("NoSuchFile")
+
+              "Sent page! \uD83D\uDE80"
+            case Failure(_: NoSuchFileException) =>
               response.statusCode = HttpStatus.notFound
-              response.send() andThen {
-                case Success(_) => println(s"Sent the 404 police \uD83D\uDE12 ($ex)")
-                case Failure(e) => fail(e)
-              }
+
+              "Sent the 404 police \uD83D\uDE12"
             case Failure(ex) =>
-              println("$ex")
               response.statusCode = HttpStatus.internalServerError
-              response.send() andThen {
-                case Success(_) => println(s"Server failure \uD83D\uDE12 ($ex)")
-                case Failure(e) => fail(e)
-              }
+
+              s"Server failure \uD83D\uDE12 ($ex)"
           }
+
+          send_or(response, msg)
         }
       },
       socket => {
@@ -71,5 +62,12 @@ object Runner {
       _ => {
         println("End of connection")
       })
+  }
+
+  def send_or(response: HttpResponse, msg: String): Unit = {
+    response.send() andThen {
+      case Success(_) => println(msg)
+      case Failure(e) => println(s"Failed: $e")
+    }
   }
 }
